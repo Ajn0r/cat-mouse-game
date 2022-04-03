365b05d18959e74f9b68921f0b55dcc40a311804
Ajn0r <ronjasjogren@hotmail.com>
2022-04-03 10:13:44 +0000

    add first structure to start game

--- run.py
+++ run.py
@@ -12,8 +12,16 @@ def welcome():
             "I will think of a word and you'll guess it,"
             " one letter at a time\n"
             "For every wrong guess the cat gets one step"
-            " closer to the mouse")
-        play = False
+            " closer to the mouse\n\n")
+
+        start_game = input("\tAre you ready to play? y/n\n")
+        if start_game.lower() == "y":
+            print("Lets play")
+        elif start_game.lower() == "n":
+            print("Bye!")
+            play = False
+        else:
+            print("Dont understand")
 
 
 welcome()
