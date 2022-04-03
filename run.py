ed6cf2ae8d4b1022ff926876bf692101999fd3ef
Ajn0r <ronjasjogren@hotmail.com>
2022-04-03 10:03:39 +0000

    add welcomephrase and info on how to play game

--- run.py
+++ run.py
@@ -2,3 +2,18 @@ def welcome():
     """
     Welcomes the player, runs once game is started.
     """
+    play = True
+    while play:
+        print(
+            "\t***********************************\n"
+            "\t Welcome to the cat and mouse game!\n"
+            "\t***********************************\n\n"
+            "Can you save the mouse from the cat?\n"
+            "I will think of a word and you'll guess it,"
+            " one letter at a time\n"
+            "For every wrong guess the cat gets one step"
+            " closer to the mouse")
+        play = False
+
+
+welcome()
